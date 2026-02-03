bd82b93a28b5a12361ba645b3a900f2060d94903
Fareedh7 <fareedhahamed07@gmail.com>
2026-02-03 22:23:04 +0530

    Created the navbar

---
+++ src/components/Titlecard/Titlecard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Titlecard.css"
+
+const Titlecard = () => {
+  return (
+    <div className='titleCard'>
+      
+    </div>
+  )
+}
+
+export default Titlecard
