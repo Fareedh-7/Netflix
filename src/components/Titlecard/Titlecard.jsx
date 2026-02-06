33f43c97ac473ddbdc08225b8b5529bd78073724
Fareedh7 <fareedhahamed07@gmail.com>
2026-02-06 08:30:42 +0530

    Created a title card section

--- src/components/Titlecard/Titlecard.jsx
+++ src/components/Titlecard/Titlecard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Titlecard.css"
+import card_data from '../../assets/cards/Cards_data'
 
 const Titlecard = () => {
   return (
-    <div className='titleCard'>
-      
+    <div className='title-card'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {card_data.map((card,index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
